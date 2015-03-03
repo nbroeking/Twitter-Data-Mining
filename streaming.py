5fbc74723f94db688e9d289450e7ffd028bab8d1
nbroeking <nbroeking@me.com>
2015-03-03 21:42:55 +0000

    Added Streaming and clollecting data

--- streaming.py
+++ streaming.py
@@ -10,13 +10,6 @@ import sys
 import sqlite3
 import time
 
-#THis is ugly but were just going to create the database right h
-db = sqlite3.connect('Twitter.db')
-# Get a cursor object
-cursor = db.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Tweets(id INTEGER PRIMARY KEY, name TEXT, geo TEXT, image TEXT, source TEXT, timestamp TEXT, text TEXT, rt INTEGER)''')
-db.commit()
-
 
 sys.path.insert(1,'/home/nbroeking/DataMining/tweepy')
 sys.path.insert(1,'/home/nbroeking//DataMining/loot/dist-packages')
@@ -28,6 +21,7 @@ from tweepy import Stream
 
 #JSON
 import json
+import ast
 
 # Consumer Keys
 consumer_key="44vVVicDrIDrA9iJ6FHCUA0TB"
@@ -36,21 +30,46 @@ consumer_secret="tyeyRqSJkHrUjec2sdO5tiIJeMb5Sk5OZVySeN9CIEoXOFcvtn"
 access_token="52314456-zHY3UfyiQ6KVrWrgMLy4nGmPzInaFmxsZVXhKCHfO"
 access_token_secret="zYsjeIH7TKxeJnHxVHwg3HVUynu4JZgKLvfhsUR0Hn7Qh"
 
+
 #Listen for tweets
 class TweetListener(StreamListener):
+    def __init__(self):
+        self.db = sqlite3.connect('Twitter.db')
+        cursor = self.db.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Tweets(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, retweeted BOOL, retweeted_count INT, time TEXT, userid INT, tweetId INT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, userid INT, followers_count INT, friendcount INT, name TEXT)''')
+        self.db.commit()
+
     def on_data(self, data):
-	
-	cursor.execute('''INSERT INTO MyTable(name, geo, image, source, timestamp, text, rt) VALUES(?,?,?,?,?,?,?)''',(tweet.user.screen_name, str(tweet.geo), tweet.user.profile_image_url, tweet.source, tweet.created_at, tweet.text, tweet.retweet_count))
-        return True
+        d = json.loads(data.strip())
+        try:
+            if d['lang'] == "en":	
+                cursor = self.db.cursor()
+            
+                cursor.execute('INSERT INTO Tweets( text, retweeted, retweeted_count, time, userid, tweetId) VALUES(?,?,?,?,?,?)', [d['text'], bool(d['retweeted']), int(d['retweet_count']), d['created_at'], int(d['user']['id']), int( d['id'])])
+                cursor.execute('INSERT INTO Users( userID, followers_count, friendcount, name) VALUES (?,?,?,?)', [d['user']['id'], d['user']['followers_count'], d['user']['friends_count'], d['user']['name']])
+            
+                self.db.commit()
+                return True
+        except: # catch *all* exceptions
+            e = sys.exc_info()[0]
+            print( e )
 
     def on_error(self, status):
         print(status)
 
 #Main Function
 if __name__ == '__main__':
-    l = TweetListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    while True:
+        try:
+            l = TweetListener()
+            auth = OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+           
+            stream = Stream(auth, l)
+            stream.filter(track=['google', 'apple', 'oracle','amazon', 'IBM', 'Google', 'Apple', 'Oracle', 'Amazon', 'ibm', 'Shell', 'shell' ])
 
-    stream = Stream(auth, l)
-    stream.filter(track=['google'])
+        except:
+            e = sys.exc_info()[0]
+            print( e)
+
