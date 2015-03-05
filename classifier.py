e530cc6ccd5845ff09bef10093ff421763d4d903
nbroeking <nbroeking@me.com>
2015-03-05 15:48:35 -0700

    Adding basic classifier

--- classifier.py
+++ classifier.py
@@ -6,40 +6,56 @@ from __future__ import absolute_import, print_function
 #Imports sys to change path
 import sys
 
+#Classifier
+import re, math, collections, itertools
+import nltk, nltk.classify.util, nltk.metrics
+from nltk.classify import NaiveBayesClassifier
+from nltk.metrics import BigramAssocMeasures
+from nltk.probability import FreqDist, ConditionalFreqDist
+
 #database
 import sqlite3
 import time
-
-
-sys.path.insert(1,'/home/nbroeking/DataMining/tweepy')
-sys.path.insert(1,'/home/nbroeking//DataMining/loot/dist-packages')
 
 #JSON
 import json
 import ast
 
 def train():
-    f = open('training.txt')
+    f = open('training.csv')
     lines = f.read().splitlines()
     
     #Create a list of training data
-    data = []
+    trainingdata = []
+    testingdata = []
+    i = 0
     for x in lines:
-        line = x.split("^")
-        if len(line) is 2:
-            data.append((line[0], line[1]))
+        line = x.split(",")
+        if len(line) is 4:
+            text = line[3].split()
+            #Process the text
+            newtext = []
+            for word in text:
+                if '@' in word:
+                    newtext.append( 'TAG')
+                elif 'http' in word:
+                    newtext.append('URL')
+                elif word.isalnum() and not len(word) < 3:
+                    newtext.append(word)
+            tup = (newtext, 'pos' if int(line[1]) else 'neg')
+            if i < 500000:
+                trainingdata.append(tup)
+            else:
+                testingdata.append(tup)
          
+    #Feature extraction
+
+
+    #Clasification
+   
+ 
+    print (data)
       
-    tweets = []  
-    #format     
-    for (words, sentiment) in data:
-        words_filtered = [word.lower() for word in words.split() if len(word) >= 3\
-        and word.isalpha()]
-        tweets.append((words_filtered, sentiment))
-
-    return tweets
-
-        
 #Main Function
 if __name__ == '__main__':
     print ("Categorizing the data")
